764c0b99b754e674dc3b5fd36a2a51a3ad5fff1a
rishitha62 <133572634+rishitha62@users.noreply.github.com>
2026-02-24 14:38:06 +0530

    Add EmployeeLeaveManagementApplication main class

---
+++ src/main/java/com/leavemanagement/EmployeeLeaveManagementApplication.java
@@ -0,0 +1,10 @@
+package com.leavemanagement;
+
+import org.springframework.boot.SpringApplication;
+
+@springBootApplication
+public class EmployeeLeaveManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeLeaveManagementApplication.class, args);
+    }
+}
