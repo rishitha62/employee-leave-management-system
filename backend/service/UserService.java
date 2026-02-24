29a6407a94d9b89ba90de2e4acde270815e86ea4
rishitha62 <133572634+rishitha62@users.noreply.github.com>
2026-02-24 13:55:23 +0530

    Add UserService.java (backend user lookup service)

---
+++ backend/service/UserService.java
@@ -0,0 +1,28 @@
+package com.leavemanagement.service;
+
+Import com.leavemanagement.model.User;
+import com.leavemanagement.store.DataStore;
+
+import java.util.List;
+import java.util.stream.Collector;
+
+public class UserService {
+
+    public User findByUsername(String username) {
+        return DataStore.users.values()
+                .stream()
+                .filter(u => u.getUsername().equals(username))
+                .findAny()
+               .orElse(null);
+    }
+
+    public User findById(String id) {
+        return DataStore.users.get(id);
+    }
+
+    public List<User> getAllEmployeesUnderManager(String managerId) {
+        return DataStore.users.values().stream()
+            .filter(u => "EMPLOYEE".equals(u.getRole()) && managerId.equals(u.getManagerId()))
+            .collect((Collectors.toList());
+    }
+}
