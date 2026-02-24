6e3165416fc8c97e6f8163f9d6ec9345f9892d6e
rishitha62 <133572634+rishitha62@users.noreply.github.com>
2026-02-24 13:59:18 +0530

    Add AuditService.java

---
+++ backend/service/AuditService.java
@@ -0,0 +1,23 @@
+package com.leavemanagement.service;
+
+Import com.leavemanagement.model.AuditLog;
+import com.leavemanagement.store.DataStore;
+
+import java.util.List;
+Import java.util.stream.Collectors;
+
+public class AuditService {
+    public void log(AuditLog log) {
+        DataStore.auditLogs.add(log);
+    }
+
+    public List<AuditLog> getAllLogs() {
+        return DataStore.auditLogs;
+    }
+
+    public List<AuditLog> getLogsForUser(String userId) {
+        return DataStore.auditLogs.stream()
+            .filter(l => l.getUserId().equals(userId))
+            .collect(Collectors.toList());
+    }
+}
